eb95707af1fb2f8ae5ef88c52e2aef2facba74d9
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2017-08-24 18:34:03 +0800

    Update devServer defaultPort 8080

--- src/mwDevServer.js
+++ src/mwDevServer.js
@@ -19,19 +19,30 @@ export default async function (context, next) {
     inline: true,
     noInfo: false,
     host: '127.0.0.1',
-    port: 9998,
+    port: 8080,
   }
 
   next()
 
   let {plugins = [], devServer} = webpackConfig
   let {host, port} = devServer
-  let uri = createDomain({port, host})
+
+  let port0 = port
+  let port1 = port + 1
+
+  if (args.port) {
+    port1 = args.port
+    port0 = args.port - 1
+  }
+
+  webpackConfig.devServer.port = port0
+
+  let uri = createDomain({port: port0, host})
 
   webpackConfig.plugins = [
     new HotModuleReplacementPlugin(),
     new NamedModulesPlugin(),
-    new BrowserSyncPlugin({host, port: args.port || 9999, proxy: uri}, {reload: true}),
+    new BrowserSyncPlugin({host, port: port1, proxy: uri}, {reload: true}),
     ...plugins,
   ]
 }
