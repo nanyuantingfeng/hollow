90701a57bd6270318e3997ff9287ce128397d90c
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2017-08-24 18:39:25 +0800

    Update devServer defaultPort 8080

--- src/mwDevServer.js
+++ src/mwDevServer.js
@@ -9,6 +9,13 @@ import {
 
 import createDomain from 'webpack-dev-server/lib/util/createDomain'
 
+function defaultTo (a, b) {
+  return a ? a : b
+}
+
+const DEFAULT_PORT = 8080
+const DEFAULT_HOST = '127.0.0.1'
+
 export default async function (context, next) {
   let {webpackConfig, args} = context
 
@@ -18,8 +25,8 @@ export default async function (context, next) {
     contentBase: false,
     inline: true,
     noInfo: false,
-    host: '127.0.0.1',
-    port: 8080,
+    host: DEFAULT_HOST,
+    port: DEFAULT_PORT,
   }
 
   next()
@@ -27,13 +34,12 @@ export default async function (context, next) {
   let {plugins = [], devServer} = webpackConfig
   let {host, port} = devServer
 
+  port = port === DEFAULT_PORT
+    ? defaultTo(args.port, port)
+    : defaultTo(port, args.port)
+
   let port0 = port
   let port1 = port + 1
-
-  if (args.port) {
-    port1 = args.port
-    port0 = args.port - 1
-  }
 
   webpackConfig.devServer.port = port0
 
