e278a677af5a3b73db2cee985d4a86943e5b0dc6
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2017-08-25 15:06:46 +0800

    code clean

---
+++ test/cases/build-jsx-multi/dist/index1.js
@@ -0,0 +1,26 @@
+webpackJsonp([1,0,2],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+function DX(props) {
+
+  console.log("http://127.0.0.1:9999");
+
+  return React.createElement(
+    "div",
+    null,
+    "14.0.0"
+  );
+}
+
+ReactDOM.render(React.createElement(
+  "div",
+  null,
+  React.createElement(DX, null)
+));
+
+/***/ })
+],[0]);
