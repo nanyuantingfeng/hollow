edc3b1d33f65762ba8d4cc1a62801b0c41da321a
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2017-08-25 11:26:57 +0800

    Update test cases

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**************************************************
+ * Created by nanyuantingfeng on 25/08/2017 11:25.
+ **************************************************/
+
+import build from './build'
+import devServer from './devServer'
+
+export {
+  build,
+  devServer,
+}
