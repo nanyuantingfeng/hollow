c1972fde18345bfc1d45b1c96ee1b6a1f2c9bf68
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2017-10-29 14:02:21 +0800

    update test cases

---
+++ test/cases/build-hash-map/dist/index-63b6cf29d5f838879f1d.js
@@ -0,0 +1,13 @@
+webpackJsonp([0],{
+
+/***/ "JkW7":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+console.log(7337);
+
+/***/ })
+
+},["JkW7"]);
---
+++ test/cases/build-normal/dist/index-222cf5bcdc1be383a524.js
@@ -0,0 +1,13 @@
+webpackJsonp([0],{
+
+/***/ "JkW7":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+console.log(1);
+
+/***/ })
+
+},["JkW7"]);
