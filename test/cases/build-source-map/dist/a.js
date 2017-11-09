3d3a483d80d05e30989ffd33a1267bf9ab48444a
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2017-11-09 13:06:24 +0800

    version 0.2.7

---
+++ test/cases/build-source-map/dist/a.js
@@ -0,0 +1,27 @@
+webpackJsonp([1],{
+
+/***/ "pJAT":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__("xHuH");
+
+console.log('a');
+
+function AAX() {}
+
+AAX.prototype.demo = function () {};
+
+/***/ }),
+
+/***/ "xHuH":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},["pJAT"]);
+//# sourceMappingURL=a.js.map
