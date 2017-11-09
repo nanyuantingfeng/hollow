d5b6b6778876fd40cb5c2059b78870bad24cda0f
nanyuantingfeng <nanyuantingfeng@aliyun.com>
2017-11-09 13:06:47 +0800

    version 0.2.7

---
+++ test/reference/build-source-map/b.js
@@ -0,0 +1,23 @@
+webpackJsonp([0],{
+
+/***/ "ihUv":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__("xHuH");
+
+console.log('b');
+
+/***/ }),
+
+/***/ "xHuH":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ })
+
+},["ihUv"]);
+//# sourceMappingURL=b.js.map
