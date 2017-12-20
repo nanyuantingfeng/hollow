efac96a6fffa66bc67568f439dbd3fb09db87ab3
nanyuantingfeng <nanyuantingfeng@sina.com>
2017-12-20 15:28:46 +0800

    delete happypack

---
+++ test/cases/build-js-lazy/index.js
@@ -0,0 +1,7 @@
+/**************************************************
+ * Created by nanyuantingfeng on 20/12/2017 14:34.
+ **************************************************/
+
+const demo = require('./demo.lazy')
+
+console.log(typeof demo)
