70f70585c41d3c25721568e51527b114b6f2970b
nanyuantingfeng <nanyuantingfeng@sina.com>
2018-02-23 19:13:47 +0800

    0.6.0 import dynamic fixed

---
+++ test/reference/build-dynamic-import/3-2b12e83d97828639897b.js
@@ -0,0 +1,52 @@
+webpackJsonp([3],{
+
+/***/ "4/+N":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "iiW8":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "pJAT":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+__webpack_require__("4/+N");
+
+var _x = __webpack_require__("CL/g");
+
+var _x2 = _interopRequireDefault(_x);
+
+var _react = __webpack_require__("SAdv");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var aaa = _react2.default.createElement(
+  'div',
+  null,
+  '111'
+); /**************************************************
+    * Created by nanyuantingfeng on 09/01/2018 12:17.
+    **************************************************/
+
+exports.default = { a: '9999999', f: (0, _x2.default)(0), aaa: aaa };
+module.exports = exports['default'];
+
+/***/ })
+
+});
