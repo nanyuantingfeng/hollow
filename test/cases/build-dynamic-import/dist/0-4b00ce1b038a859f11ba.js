e0ab00e2e26556978a7192c24207323123e7bd86
nanyuantingfeng <nanyuantingfeng@sina.com>
2018-05-16 19:38:53 +0800

    update webpack 4.0

---
+++ test/cases/build-dynamic-import/dist/0-4b00ce1b038a859f11ba.js
@@ -0,0 +1,69 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "MyQS":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "R8FN":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+__webpack_require__("MyQS");
+
+var _x = __webpack_require__("viiV");
+
+var _x2 = _interopRequireDefault(_x);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**************************************************
+ * Created by nanyuantingfeng on 09/01/2018 13:39.
+ **************************************************/
+
+exports.default = { d: '9999999', f: (0, _x2.default)(323232) };
+module.exports = exports['default'];
+
+/***/ }),
+
+/***/ "uu4z":
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "viiV":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = demoFunction;
+
+__webpack_require__("uu4z");
+
+function demoFunction(demo) {
+  return function (dd) {
+    return demo + dd;
+  };
+} /**************************************************
+   * Created by nanyuantingfeng on 09/01/2018 13:04.
+   **************************************************/
+
+module.exports = exports['default'];
+
+/***/ })
+
+}]);
