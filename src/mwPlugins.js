659013f812c4548ddcf6d7410abe43956dc5a86d
nanyuantingfeng <nanyuantingfeng@sina.com>
2018-06-24 14:45:43 +0800

    delete  splitChunks

--- src/mwPlugins.js
+++ src/mwPlugins.js
@@ -1,7 +1,7 @@
 /**************************************************
  * Created by nanyuantingfeng on 27/10/2017 16:25.
  **************************************************/
-import path from 'path'
+import path from 'path';
 
 import {
   MiniCssExtractPlugin,
@@ -9,9 +9,9 @@ import {
   ProgressPlugin,
   HashedModuleIdsPlugin,
   AggressiveSplittingPlugin,
-} from './plugins'
+} from './plugins';
 
-import { notifier, fnProgressHandler } from './util'
+import { notifier, fnProgressHandler } from './util';
 
 export default async function (context, next) {
   context.plugins = [
@@ -23,57 +23,58 @@ export default async function (context, next) {
             message: 'warn',
             contentImage: path.join(__dirname, '../assets/warn.png'),
             sound: 'Glass',
-          })
-          return
+          });
+          return;
         }
-        const error = errors[0]
+        const error = errors[0];
         notifier.notify({
           title: 'hollow cli',
           message: `${severity} : ${error ? error.name : error}`,
           subtitle: error ? error.file : error || '',
           contentImage: path.join(__dirname, '../assets/fail.png'),
           sound: 'Glass',
-        })
+        });
       },
     }),
     new ProgressPlugin(fnProgressHandler),
     new HashedModuleIdsPlugin(),
-  ]
+  ];
 
-  next()
+  next();
 
-  const { hash, compress, plugins, dll } = context
+  const {hash, compress, plugins, dll} = context;
   if (!Array.isArray(dll)) {
-    context.webpackConfig.optimization.splitChunks = {
+/*    context.webpackConfig.optimization.splitChunks = {
       chunks: 'async',
-      minSize: 244 * 1024,
+      minSize: 30000,
       minChunks: 1,
       maxAsyncRequests: 5,
       maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
       name: true,
       cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
         default: {
           minChunks: 2,
           priority: -20,
-          reuseExistingChunk: true,
-        },
-        vendors: {
-          test: /[/]node_modules[/]/,
-          priority: -10
+          reuseExistingChunk: true
         }
       }
-    }
+    };*/
   }
 
-  const filename = hash ? '[hash]-[name].css' : '[name].css'
-  const chunkFilename = hash ? '[hash]-[id].css' : '[id].css'
+  const filename = hash ? '[hash]-[name].css' : '[name].css';
+  const chunkFilename = hash ? '[hash]-[id].css' : '[id].css';
 
   plugins.push(new MiniCssExtractPlugin({
     filename,
     chunkFilename,
-  }))
+  }));
 
-  context.webpackConfig.optimization.minimize = !!compress
+  context.webpackConfig.optimization.minimize = !!compress;
 
   // dll 模式下不能使用当前插件
   if (!Array.isArray(dll)) {
@@ -82,10 +83,10 @@ export default async function (context, next) {
       maxSize: 800 * 1024,
       chunkOverhead: 0,
       entryChunkMultiplicator: 1,
-    }))
+    }));
   }
 
-  context.plugins = plugins
+  context.plugins = plugins;
 
   //context.webpackConfig.recordsOutputPath = path.join(cwd, 'build', 'records.json')
 }
