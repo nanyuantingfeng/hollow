40b6e90b894ea9a9acb77fad619ef16ce78f5673
nanyuantingfeng <nanyuantingfeng@sina.com>
2018-08-22 11:36:33 +0800

    code clean

---
+++ test/cases/build-dynamic-import/dist/1-7d8d094d4648a3450f33.js
@@ -0,0 +1,77 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "KpHw":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+__webpack_require__("hBaP");
+
+var _x = __webpack_require__("viiV");
+
+var _x2 = _interopRequireDefault(_x);
+
+var _react = __webpack_require__("r0ML");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var aaa = _react2.default.createElement(
+  'div',
+  null,
+  '111'
+); /**************************************************
+    * Created by nanyuantingfeng on 09/01/2018 12:17.
+    **************************************************/
+
+exports.default = { a: '9999999', f: (0, _x2.default)(0), aaa: aaa };
+module.exports = exports['default'];
+
+/***/ }),
+
+/***/ "hBaP":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "uu4z":
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ "viiV":
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+__webpack_require__("uu4z");
+
+function demoFunction(demo) {
+  return function (dd) {
+    return demo + dd;
+  };
+} /**************************************************
+   * Created by nanyuantingfeng on 09/01/2018 13:04.
+   **************************************************/
+
+exports.default = demoFunction;
+module.exports = exports['default'];
+
+/***/ })
+
+}]);
