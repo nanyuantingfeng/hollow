2c150531c71debfe3ce23988b0141a63fcfbf189
nanyuantingfeng <nanyuantingfeng@163.com>
2018-10-17 10:51:23 +0800

    update dependencies  version

---
+++ test/cases/build-jsx-multi/dist/precache-manifest.7b4160e09d1675fda12eeb7fa5218a4d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "2930e6caad95dfad928d76fc1eb2e003",
+    "url": "mime.js"
+  },
+  {
+    "revision": "8f081b39b99be5fb1e8f37810e51aeb1",
+    "url": "index2.html"
+  },
+  {
+    "revision": "f48537d523ef34a971e4",
+    "url": "runtime~index2.js"
+  },
+  {
+    "revision": "5ca6ad6d8125c4293bcd",
+    "url": "runtime~index0.js"
+  },
+  {
+    "revision": "c47239e8eee03cae7aa5",
+    "url": "runtime~index1.js"
+  },
+  {
+    "revision": "823684531e82194d7152",
+    "url": "index2.chunk.js"
+  },
+  {
+    "revision": "8878c9d07d17e534c42af59ec6cbaf32",
+    "url": "index1.html"
+  },
+  {
+    "revision": "fbc6e3dfcd0e08647700",
+    "url": "index1.chunk.js"
+  },
+  {
+    "revision": "25f4911f583ec01aa53de6912b9ab961",
+    "url": "index0.html"
+  },
+  {
+    "revision": "8b711b61b6277b1be12f",
+    "url": "index0.chunk.js"
+  },
+  {
+    "revision": "4348138e7d643fa97086f186a829a5a0",
+    "url": "fetch.js"
+  },
+  {
+    "revision": "afcfe9ef72ffb97e48b1a978c84d2a19",
+    "url": "favicon.ico"
+  }
+];
